82fbfda2d560055c4d18236d38c0b2c22f8f9302
zhe.li <zhe.li@ihandysoft.com>
2021-05-27 11:58:54 +0800

    JSON Press - info 类型修改 html 结构和 css 样式以实现支持 inline 配置

--- src/editors/info.js
+++ src/editors/info.js
@@ -5,8 +5,10 @@ export class InfoEditor extends ButtonEditor {
   build () {
     this.options.compact = false
     this.header = this.label = this.theme.getFormInputLabel(this.getTitle())
-    this.description = this.theme.getDescription(this.schema.description || '')
-    this.control = this.theme.getFormControl(this.label, this.description, null)
+    this.input = this.theme.getDescription(this.schema.description || '')
+    this.label.classList.add('info-label')
+    this.input.classList.add('info-container')
+    this.control = this.theme.getFormControl(this.label, this.input, null)
     this.container.appendChild(this.control)
   }
 
